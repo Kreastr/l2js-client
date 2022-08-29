7cdff69b04266257fa0fa58ff0039e1df044f265
Aleksei Romanenko <aleksei.romanenko@pm.me>
2022-08-29 19:40:23 +0300

    Item packets

--- src/network/incoming/game/GetItem.ts
+++ src/network/incoming/game/GetItem.ts
@@ -1,14 +1,22 @@
 import GameClientPacket from "./GameClientPacket";
 
 export default class GetItem extends GameClientPacket {
+  objId : number = 0;
+  x: number = 0;
+  y: number = 0;
+  z: number = 0;
+  
   // @Override
   readImpl(): boolean {
     const _id = this.readC();
 
     const _playerId = this.readD();
-    const _objId = this.readD();
+    this.objId = this.readD();
 
     const [x, y, z] = this.readLoc();
+    this.x = x;
+    this.y = y;
+    this.z = z;
 
     return true;
   }
